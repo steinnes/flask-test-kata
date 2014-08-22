2a4efbdcec8708253c53dc8db72b676400ef3aa6
Steinn Eldjárn Sigurðarson <steinnes@gmail.com>
2014-08-22 12:42:08 +0000

    Implement Unit 5
    logic.Calculator contains another method stub: div. This method should
    divide a by b, but it is just a stub now. Write corresponding tests for
    the method, as you've written for logic.Calculator.mul, then implement
    the method to pass your tests.

--- calculator/logic.py
+++ calculator/logic.py
@@ -14,7 +14,7 @@ class Calculator(object):
         self.min_value = min_value
         self.max_value = max_value
 
-    def mul(self, a, b):
+    def _protect_boundaries(self, a, b):
         if a > self.max_value:
             raise ValueTooHighException("Input {} larger than maximum allowed: {}".format(a, self.max_value))
         if a < self.min_value:
@@ -24,7 +24,10 @@ class Calculator(object):
         if b < self.min_value:
             raise ValueTooLowException("Input {} smaller than minimum allowed: {}".format(b, self.max_value))
 
+    def mul(self, a, b):
+        self._protect_boundaries(a, b)
         return a * b
 
     def div(self, a, b):
-        pass
+        self._protect_boundaries(a, b)
+        return float(a) / float(b);
--- tests/unit/test_logic.py
+++ tests/unit/test_logic.py
@@ -19,25 +19,25 @@ class CalculatorTests(TestCase):
         result = calc.mul(-3, 4)
         self.assertEquals(result, -12)
 
-    def test_mul_raises_too_low_exception_on_a_too_low(self):
+    def test_protect_boundaries_on_a_too_low(self):
         calc = Calculator(100, 1000)
         with pytest.raises(ValueTooLowException):
-            calc.mul(1, 101)
+            calc._protect_boundaries(1, 101)
 
-    def test_mul_raises_too_low_exception_on_b_too_low(self):
+    def test_protect_boundaries_on_b_too_low(self):
         calc = Calculator(100, 1000)
         with pytest.raises(ValueTooLowException):
-            calc.mul(101, 1)
+            calc._protect_boundaries(101, 1)
 
-    def test_mul_raises_too_high_exception_on_a_too_high(self):
+    def test_protect_boundaries_on_a_too_high(self):
         calc = Calculator(1, 10)
         with pytest.raises(ValueTooHighException):
-            calc.mul(11, 5)
+            calc._protect_boundaries(11, 5)
 
-    def test_mul_raises_too_high_exception_on_b_too_high(self):
+    def test_protect_boundaries_on_b_too_high(self):
         calc = Calculator(1, 10)
         with pytest.raises(ValueTooHighException):
-            calc.mul(5, 11)
+            calc._protect_boundaries(5, 11)
 
     def test_mul_does_not_raise_exception_on_a_on_boundaries(self):
         calc = Calculator(1, 10)
@@ -59,6 +59,41 @@ class CalculatorTests(TestCase):
         result = calc.mul(5, 10)
         self.assertEquals(result, 50)
 
+    def test_div_with_two_positive_numbers(self):
+        calc = Calculator()
+        result = calc.div(12, 3)
+        self.assertEquals(result, 4)
+        
+    def test_div_with_two_negative_numbers(self):
+        calc = Calculator()
+        result = calc.div(-12, -3)
+        self.assertEquals(result, 4)
 
-    def test_div(self):
-        pass
+    def test_div_with_one_negative_one_positive_number(self):
+        calc = Calculator()
+        result = calc.div(-12, 3)
+        self.assertEquals(result, -4)
+
+
+    def test_div_does_not_raise_exception_on_a_on_boundaries(self):
+        calc = Calculator(1, 10)
+
+        # test lower bound
+        result = calc.div(1, 2)
+        self.assertEquals(result, 0.5)
+
+        # test upper bound
+        result = calc.div(10, 5)
+        self.assertEquals(result, 2)
+
+    def test_div_does_not_raise_exception_on_b_on_boundaries(self):
+        calc = Calculator(2, 10)
+
+        # test lower bound
+        result = calc.div(10, 2)
+        self.assertEquals(result, 5)
+
+        # test upper bound
+        result = calc.div(5, 10)
+        self.assertEquals(result, 0.5)
+
