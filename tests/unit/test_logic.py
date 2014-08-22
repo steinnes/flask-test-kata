350a0de52e619af97343f206c576aefb8eb538a6
Steinn Eldjárn Sigurðarson <steinnes@gmail.com>
2014-08-22 12:18:27 +0000

    Implement Unit 2
    test_mul as a test is rather coarse. A better test would be in the form
    test_function_satisfies_condition_after_pre-condition. Implement the
    tests test_mul_with_two_positive_numbers,
    test_mul_with_two_negative_numbers,
    test_mul_with_one_negative_one_positive_number.

--- tests/unit/test_logic.py
+++ tests/unit/test_logic.py
@@ -3,10 +3,20 @@ from calculator.logic import Calculator
 
 
 class CalculatorTests(TestCase):
-    def test_mul(self):
+    def test_mul_with_two_positive_numbers(self):
         calc = Calculator()
         result = calc.mul(3, 4)
         self.assertEquals(result, 12)
+        
+    def test_mul_with_two_negative_numbers(self):
+        calc = Calculator()
+        result = calc.mul(-3, -4)
+        self.assertEquals(result, 12)
+
+    def test_mul_with_one_negative_one_positive_number(self):
+        calc = Calculator()
+        result = calc.mul(-3, 4)
+        self.assertEquals(result, -12)
 
     def test_div(self):
         pass
