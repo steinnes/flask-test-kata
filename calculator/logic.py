0feedae50e389cbc5837fd2c9e7dc46076aa8f03
Haukur Rosinkranz <hauxir@gmail.com>
2017-04-24 13:29:37 +0000

    steps 1,2,3 done

--- calculator/logic.py
+++ calculator/logic.py
@@ -14,8 +14,16 @@ class Calculator(object):
         self.min_value = min_value
         self.max_value = max_value
 
+    def _checkvals(self, a, b):
+        if a < self.min_value or b < self.min_value:
+            raise ValueTooLowException()
+        if a > self.max_value or b > self.max_value:
+            raise ValueTooHighException()
+
     def mul(self, a, b):
-        pass
+        self._checkvals(a, b)
+        return a*b
 
     def div(self, a, b):
+        self._checkvals(a, b)
         pass
--- tests/unit/test_logic.py
+++ tests/unit/test_logic.py
@@ -1,10 +1,45 @@
+import pytest
 from unittest import TestCase
-from calculator.logic import Calculator
+
+from calculator.logic import Calculator, ValueTooLowException, ValueTooHighException
 
 
 class CalculatorTests(TestCase):
     def test_mul(self):
-        pass
+        calc = Calculator()
+        assert calc.mul(6, 5) == 30
+
+    def test_mul_with_two_positive_numbers(self):
+        calc = Calculator()
+        assert calc.mul(6, 5) == 30
+
+    def test_mul_with_two_negative_numbers(self):
+        calc = Calculator()
+        assert calc.mul(-1, -1) == 1
+
+    def test_mul_with_one_negative_one_positive_number(self):
+        calc = Calculator()
+        assert calc.mul(-1, 1) == -1
+
+    def test_mul_with_first_value_too_low(self):
+        with pytest.raises(ValueTooLowException):
+            calc = Calculator(1, 10)
+            calc.mul(0, 2)
+
+    def test_mul_with_first_value_too_high(self):
+        with pytest.raises(ValueTooHighException):
+            calc = Calculator(1, 10)
+            calc.mul(11, 2)
+
+    def test_mul_with_second_value_too_low(self):
+        with pytest.raises(ValueTooLowException):
+            calc = Calculator(1, 10)
+            calc.mul(2, 0)
+
+    def test_mul_with_second_value_too_high(self):
+        with pytest.raises(ValueTooHighException):
+            calc = Calculator(1, 10)
+            calc.mul(2, 11)
 
     def test_div(self):
         pass
